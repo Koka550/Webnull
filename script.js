680254b13b0abac3e0b6096f66bcd0672bcd7efb
Koka550 <mohdtdse@gmail.com>
2020-06-19 21:07:35 +0200

    commit on master

--- script.js
+++ script.js
@@ -9,11 +9,42 @@ function subtract(a , b) {
 }
 function sum() {
     let numbers = [] ;
+    let result ;
     while(true) {
         let input = prompt("Enter a number or press 'S' to submit") ;
         if(input === "s" || input === null) {
             break;
         }
-        numbers.push(Number(numbers)) ;
+        numbers.push(Number(input)) ;
     }
+    let submaition = numbers.reduce(myfun , 0); 
+    function myfun(a , b) {
+     return a + b ;
+    
+    }
+    return submaition ;
 }
+function multiply() {
+    let numbers = [] ;
+    let result ;
+    while(true) {
+        let input = prompt("Enter a number or press 'S' to submit") ;
+        if(input === "s" || input === null) {
+            break;
+        }
+        numbers.push(Number(input)) ;
+    }
+    let submaition = numbers.reduce(myfun , 0); 
+    function myfun(a , b) {
+     return a * b ;
+    
+    }
+    return submaition ;
+}
+function power(theNum , thePower) {
+    let result = Math.pow(theNum , thePower) ;
+    return result ;
+}
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
